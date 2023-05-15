9bf2144d162a8f7002de891d01004dd691b244fc
gabilang12 <gabigomezcruz@hotmail.com>
2023-05-15 18:49:25 -0500

    migración vista iniciar sesión

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nuevo_proyecto_final/views/iniciar_sesion.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    //home: const IniciarSesion()
+    home: const IniciarSesion()
   );
   }
 }
---
+++ lib/views/iniciar_sesion.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:nuevo_proyecto_final/views/home.dart';
+
+class IniciarSesion extends StatefulWidget {
+  const IniciarSesion({super.key});
+
+  @override
+  State<IniciarSesion> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<IniciarSesion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text(
+                'Clasificador de Imágenes',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+          ),
+          const SizedBox(height: 40,),
+          const Center(
+            child: Text(
+                'USUARIO',
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+          ),
+          const SizedBox(height: 10,),
+          SizedBox(
+            width: 300,
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Nombre de Usuario',
+              ),
+              onSaved: (String? value) {},
+              validator: (String? value) {
+                return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+              },
+            ),
+          ),
+          const SizedBox(height: 30,),
+          const Center(
+            child: Text(
+                'CONTRASEÑA',
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+          ),
+          const SizedBox(height: 10,),
+          SizedBox(
+            width: 300,
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Contraseña',
+              ),
+              onSaved: (String? value) {},
+              validator: (String? value) {
+                return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+              },
+              obscureText: true
+            ),
+          ),
+          const SizedBox(height: 40,),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context); //ir
+              Navigator.push( //volver
+                context, 
+                MaterialPageRoute(builder: (_) => const Home())
+              );
+            },
+            child: const Text(
+              'Iniciar sesión', 
+              style: TextStyle(fontSize: 25),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
