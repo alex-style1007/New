c7d04a01d209aaa27406e4b8c6d5c9ded1b31bda
gabilang12 <gabigomezcruz@hotmail.com>
2023-05-15 20:50:20 -0500

    Boton regresar en vista clasificar

--- lib/views/cargar_imagenes.dart
+++ lib/views/cargar_imagenes.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:nuevo_proyecto_final/views/home.dart';
 
 class CargarImagenes extends StatelessWidget {
   const CargarImagenes({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+       appBar: AppBar(
+        title: const Text('Clasificar Imágenes'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              width: 250, 
+              height: 50, 
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); //ir
+                  Navigator.push( //volver
+                    context, 
+                    MaterialPageRoute(builder: (_) => const Home())
+                  );
+                },
+                child: const Text(
+                  'REGRESAR',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ),
+          ),
+        ]
+      )
+    );
   }
 }
