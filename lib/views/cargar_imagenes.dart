ed824e9d00b607584286ce0ca6ea33ae095d2de8
gabilang12 <gabigomezcruz@hotmail.com>
2023-05-18 17:47:55 -0500

    update cargar imagenes: diseño completo

--- lib/views/cargar_imagenes.dart
+++ lib/views/cargar_imagenes.dart
@@ -1,132 +1,56 @@
+//ignore_for_file: deprecated_member_use
+
 import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:video_player/video_player.dart';
+import 'package:nuevo_proyecto_final/views/home.dart';
 
 class Picker extends StatefulWidget {
-  const Picker({super.key});
+  const Picker({Key? key}) : super(key: key);
 
   @override
-  State<Picker> createState() => _MyHomePageState();
+  State<Picker> createState() => _PickerState();
 }
 
-class _MyHomePageState extends State<Picker> {
-  List<XFile>? _imageFileList;
-
-  void _setImageFileListFromFile(XFile? value) {
-    _imageFileList = value == null ? null : <XFile>[value];
-  }
-
-  dynamic _pickImageError;
-  bool isVideo = false;
-
-  VideoPlayerController? _controller;
-  VideoPlayerController? _toBeDisposed;
-  String? _retrieveDataError;
-
+class _PickerState extends State<Picker> {
   final ImagePicker _picker = ImagePicker();
-  final TextEditingController maxWidthController = TextEditingController();
-  final TextEditingController maxHeightController = TextEditingController();
-  final TextEditingController qualityController = TextEditingController();
-
-  Future<void> _playVideo(XFile? file) async {
-    if (file != null && mounted) {
-      await _disposeVideoController();
-      late VideoPlayerController controller;
-      if (kIsWeb) {
-        controller = VideoPlayerController.network(file.path);
-      } else {
-        controller = VideoPlayerController.file(File(file.path));
-      }
-      _controller = controller;
-      const double volume = kIsWeb ? 0.0 : 1.0;
-      await controller.setVolume(volume);
-      await controller.initialize();
-      await controller.setLooping(true);
-      await controller.play();
-      setState(() {});
-    }
-  }
+  List<XFile>? _imageFileList;
+  dynamic _pickImageError;
 
   Future<void> _onFileButtonPressed() async {
-    final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    XFile? pickedFile;
+    try {
+      pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    } catch (e) {
+      setState(() {
+        _pickImageError = e;
+      });
+    }
     setState(() {
-      _setImageFileListFromFile(pickedFile);
+      _imageFileList = pickedFile == null ? null : <XFile>[pickedFile];
     });
   }
 
-  @override
-  void deactivate() {
-    if (_controller != null) {
-      _controller!.setVolume(0.0);
-      _controller!.pause();
-    }
-    super.deactivate();
-  }
-
-  @override
-  void dispose() {
-    _disposeVideoController();
-    maxWidthController.dispose();
-    maxHeightController.dispose();
-    qualityController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _disposeVideoController() async {
-    if (_toBeDisposed != null) {
-      await _toBeDisposed!.dispose();
-    }
-    _toBeDisposed = _controller;
-    _controller = null;
-  }
-
-  Widget _previewVideo() {
-    final Text? retrieveError = _getRetrieveErrorWidget();
-    if (retrieveError != null) {
-      return retrieveError;
-    }
-    if (_controller == null) {
-      return const Text(
-        'You have not yet picked a video',
-        textAlign: TextAlign.center,
-      );
-    }
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: AspectRatioVideo(_controller),
-    );
-  }
-
   Widget _previewImages() {
-    final Text? retrieveError = _getRetrieveErrorWidget();
-    if (retrieveError != null) {
-      return retrieveError;
-    }
     if (_imageFileList != null) {
-      return Semantics(
-        label: 'image_picker_example_picked_images',
-        child: ListView.builder(
-          key: UniqueKey(),
-          itemBuilder: (BuildContext context, int index) {
-            return Semantics(
-              label: 'image_picker_example_picked_image',
-              child: kIsWeb
-                  ? Image.network(_imageFileList![index].path)
-                  : Image.file(
-                      File(_imageFileList![index].path),
-                      errorBuilder: (BuildContext context, Object error,
-                              StackTrace? stackTrace) =>
-                          const Center(
-                              child: Text('This image type is not supported')),
-                    ),
-            );
-          },
-          itemCount: _imageFileList!.length,
-        ),
+      return ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return kIsWeb
+              ? Image.network(
+                  _imageFileList![index].path,
+                  height: 300,
+                  width: 400,
+                )
+              : Image.file(
+                  File(_imageFileList![index].path),
+                  height: 300,
+                  width: 400,
+                );
+        },
+        itemCount: _imageFileList!.length,
       );
     } else if (_pickImageError != null) {
       return Text(
@@ -141,287 +65,64 @@ class _MyHomePageState extends State<Picker> {
     }
   }
 
-  Widget _handlePreview() {
-    if (isVideo) {
-      return _previewVideo();
-    } else {
-      return _previewImages();
-    }
-  }
-
-  Future<void> retrieveLostData() async {
-    final LostDataResponse response = await _picker.retrieveLostData();
-    if (response.isEmpty) {
-      return;
-    }
-    if (response.file != null) {
-      if (response.type == RetrieveType.video) {
-        isVideo = true;
-        await _playVideo(response.file);
-      } else {
-        isVideo = false;
-        setState(() {
-          if (response.files == null) {
-            _setImageFileListFromFile(response.file);
-          } else {
-            _imageFileList = response.files;
-          }
-        });
-      }
-    } else {
-      _retrieveDataError = response.exception!.code;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cargar imagenes'),
+        title: const Text('Cargar Imágenes'),
       ),
-      body: Center(child: 
-        !kIsWeb && defaultTargetPlatform == TargetPlatform.android ? FutureBuilder<void>(
-          future: retrieveLostData(),
-          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return const Text(
-                  'You have not yet picked an image.',
-                  textAlign: TextAlign.center,
-                );
-              case ConnectionState.done:
-                return _handlePreview();
-              case ConnectionState.active:
-                if (snapshot.hasError) {
-                  return Text(
-                    'Pick image/video error: ${snapshot.error}}',
-                    textAlign: TextAlign.center,
-                  );
-                } else {
-                  return const Text(
-                    'No has cargado ninguna imagen',
-                    textAlign: TextAlign.center,
-                  );
-                }
-            }
-          },
-        ): _handlePreview(),
-      ),
-      floatingActionButton: Row(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
-            width: 200,
+          Center(
+            child: Container(
+              height: 300,
+              width: 400,
+              alignment: Alignment.center,
+              child: _previewImages(),
+            ),
+          ),
+          const SizedBox(height: 20,),
+          Center(
             child: ElevatedButton(
-              onPressed: _onFileButtonPressed, // Llama a la nueva función _onFileButtonPressed(),q
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(3.0),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  SizedBox(width: 10),
-                  Text('Cargar imagen'),
-                ],
-              ),
+              onPressed: _onFileButtonPressed,
+              style: ElevatedButton.styleFrom(primary: const Color.fromARGB(255, 169, 173, 175)),
+              child: const Text('Seleccionar imagen'),
+            ),
+          ),
+          const SizedBox(height: 40,),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); //ir
+                Navigator.push( //volver
+                  context, 
+                  MaterialPageRoute(builder: (_) => const Home())
+                );
+              },
+              style: ElevatedButton.styleFrom(primary: const Color.fromARGB(255, 169, 173, 175)),
+              child: const Text('Clasificar'),
+            ),
+          ),
+          const SizedBox(height: 40,),
+          const Center(
+            child: Text('Clase: _______________')
+          ),
+          const SizedBox(height: 40,),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); //ir
+                Navigator.push( //volver
+                  context, 
+                  MaterialPageRoute(builder: (_) => const Home())
+                );
+              },
+              child: const Text('Regresar'),
             ),
           ),
         ],
       ),
     );
   }
-
-  Text? _getRetrieveErrorWidget() {
-    if (_retrieveDataError != null) {
-      final Text result = Text(_retrieveDataError!);
-      _retrieveDataError = null;
-      return result;
-    }
-    return null;
-  }
 }
-
-typedef OnPickImageCallback = void Function(
-    double? maxWidth, double? maxHeight, int? quality);
-
-class AspectRatioVideo extends StatefulWidget {
-  const AspectRatioVideo(this.controller, {super.key});
-
-  final VideoPlayerController? controller;
-
-  @override
-  AspectRatioVideoState createState() => AspectRatioVideoState();
-}
-
-class AspectRatioVideoState extends State<AspectRatioVideo> {
-  VideoPlayerController? get controller => widget.controller;
-  bool initialized = false;
-
-  void _onVideoControllerUpdate() {
-    if (!mounted) {
-      return;
-    }
-    if (initialized != controller!.value.isInitialized) {
-      initialized = controller!.value.isInitialized;
-      setState(() {});
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    controller!.addListener(_onVideoControllerUpdate);
-  }
-
-  @override
-  void dispose() {
-    controller!.removeListener(_onVideoControllerUpdate);
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (initialized) {
-      return Center(
-        child: AspectRatio(
-          aspectRatio: controller!.value.aspectRatio,
-          child: VideoPlayer(controller!),
-        ),
-      );
-    } else {
-      return Container();
-    }
-  }
-}
-
-//ignore_for_file: deprecated_member_use
-
-// import 'dart:async';
-// import 'dart:io';
-
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'package:nuevo_proyecto_final/views/home.dart';
-
-// class Picker extends StatefulWidget {
-//   const Picker({Key? key}) : super(key: key);
-
-//   @override
-//   State<Picker> createState() => _PickerState();
-// }
-
-// class _PickerState extends State<Picker> {
-//   final ImagePicker _picker = ImagePicker();
-//   List<XFile>? _imageFileList;
-//   dynamic _pickImageError;
-
-//   Future<void> _onFileButtonPressed() async {
-//     XFile? pickedFile;
-//     try {
-//       pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-//     } catch (e) {
-//       setState(() {
-//         _pickImageError = e;
-//       });
-//     }
-//     setState(() {
-//       _imageFileList = pickedFile == null ? null : <XFile>[pickedFile];
-//     });
-//   }
-
-//   Widget _previewImages() {
-//     if (_imageFileList != null) {
-//       return ListView.builder(
-//         itemBuilder: (BuildContext context, int index) {
-//           return kIsWeb
-//               ? Image.network(
-//                   _imageFileList![index].path,
-//                   height: 300,
-//                   width: 400,
-//                 )
-//               : Image.file(
-//                   File(_imageFileList![index].path),
-//                   height: 300,
-//                   width: 400,
-//                 );
-//         },
-//         itemCount: _imageFileList!.length,
-//       );
-//     } else if (_pickImageError != null) {
-//       return Text(
-//         'Pick image error: $_pickImageError',
-//         textAlign: TextAlign.center,
-//       );
-//     } else {
-//       return const Text(
-//         'No has cargado ninguna imagen.',
-//         textAlign: TextAlign.center,
-//       );
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Cargar Imágenes'),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Center(
-//             child: Container(
-//               height: 300,
-//               width: 400,
-//               alignment: Alignment.center,
-//               child: _previewImages(),
-//             ),
-//           ),
-//           const SizedBox(height: 20,),
-//           Center(
-//             child: ElevatedButton(
-//               onPressed: _onFileButtonPressed,
-//               style: ElevatedButton.styleFrom(primary: const Color.fromARGB(255, 169, 173, 175)),
-//               child: const Text('Seleccionar imagen'),
-//             ),
-//           ),
-//           const SizedBox(height: 40,),
-//           Center(
-//             child: ElevatedButton(
-//               onPressed: () {
-//                 Navigator.pop(context); //ir
-//                 Navigator.push( //volver
-//                   context, 
-//                   MaterialPageRoute(builder: (_) => const Home())
-//                 );
-//               },
-//               style: ElevatedButton.styleFrom(primary: const Color.fromARGB(255, 169, 173, 175)),
-//               child: const Text('Clasificar'),
-//             ),
-//           ),
-//           const SizedBox(height: 40,),
-//           const Center(
-//             child: Text('Clase: _______________')
-//           ),
-//           const SizedBox(height: 40,),
-//           Center(
-//             child: ElevatedButton(
-//               onPressed: () {
-//                 Navigator.pop(context); //ir
-//                 Navigator.push( //volver
-//                   context, 
-//                   MaterialPageRoute(builder: (_) => const Home())
-//                 );
-//               },
-//               child: const Text('Regresar'),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
