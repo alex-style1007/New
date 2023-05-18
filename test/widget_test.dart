a5823d8f071a088e5271772ab73151b59f8fcfa4
Felipebj18 <felipebuitragoj@gmail.com>
2023-05-18 13:07:13 -0500

    arerglando importación de File

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nuevo_proyecto_final/main.dart';
+import 'package:nuevo_proyecto_final/views/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
